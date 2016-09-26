6bbb517f0e5630e3b18026866d01302bc059e07b
Logan Stafman <loganstafman@gmail.com>
2016-09-26 15:48:45 -0400

    adding sparkpi

--- examples/src/main/scala/org/apache/spark/examples/SparkPi.scala
+++ examples/src/main/scala/org/apache/spark/examples/SparkPi.scala
@@ -29,14 +29,60 @@ object SparkPi {
       .builder
       .appName("Spark Pi")
       .getOrCreate()
-    val slices = if (args.length > 0) args(0).toInt else 2
-    val n = math.min(100000L * slices, Int.MaxValue).toInt // avoid overflow
-    val count = spark.sparkContext.parallelize(1 until n, slices).map { i =>
-      val x = random * 2 - 1
-      val y = random * 2 - 1
-      if (x*x + y*y < 1) 1 else 0
-    }.reduce(_ + _)
-    println("Pi is roughly " + 4.0 * count / (n - 1))
+    val sc = new SparkContext()
+    sc.addSchedulablePool("sparkpi1",1,1)
+    sc.addSchedulablePool("sparkpi2",1,1)
+    val t1 = new Thread{
+        override def run() {
+          sc.setLocalProperty("spark.scheduler.pool","sparkpi1")
+          val slices = if (args.length > 0) args(0).toInt else 2
+          var countTotal = 0
+          var piApprox = 0
+          for(i <- 1 to 1000) {
+            val startTime = System.currentTimeMillis()
+            val n = math.min(100000L * slices, Int.MaxValue).toInt // avoid overflow
+            val count = spark.sparkContext.parallelize(1 until n, slices).map { i =>
+              val x = random * 2 - 1
+              val y = random * 2 - 1
+              if (x*x + y*y < 1) 1 else 0
+            }.reduce(_ + _)
+          countTotal += count
+          val newPi = 4.0 * countTotal / ((n - 1) * i)
+          val dPi = Math.abs(dPi - piApprox)
+          piApprox = newPi
+          val t = System.currentTimeMillis() - startTime
+          val newWeight = dPi / t
+          sc.setPoolWeight("sparkpi1",newWeight)
+          }
+        }
+        println(s"Pi1 is approximately $piApprox")
+    }.start()
+    val t2 = new Thread{
+        override def run() {
+          sc.setLocalProperty("spark.scheduler.pool","sparkpi2")
+          val slices = if (args.length > 0) args(0).toInt else 2
+          var countTotal = 0
+          var piApprox = 0
+          for(i <- 1 to 1000) {
+            val startTime = System.currentTimeMillis()
+            val n = math.min(100000L * slices, Int.MaxValue).toInt // avoid overflow
+            val count = spark.sparkContext.parallelize(1 until n, slices).map { i =>
+              val x = random * 2 - 1
+              val y = random * 2 - 1
+              if (x*x + y*y < 1) 1 else 0
+            }.reduce(_ + _)
+          countTotal += count
+          val newPi = 4.0 * countTotal / ((n - 1) * i)
+          val dPi = Math.abs(dPi - piApprox)
+          piApprox = newPi
+          val t = System.currentTimeMillis() - startTime
+          val newWeight = dPi / t
+          sc.setPoolWeight("sparkpi2",newWeight)
+          }
+        }
+        println(s"Pi2 is approximately $piApprox")
+    }.start()
+    
     spark.stop()
   }
 }
