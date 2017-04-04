c1b3af6da72b2ad4d082c74524c246249a0a8cd6
Andrew Or <andrewor14@gmail.com>
2017-04-04 16:40:50 -0400

    Make plotting code match printed logs

--- plotting/plot.py
+++ plotting/plot.py
@@ -13,41 +13,40 @@ if len(args) != 2:
 log_file_name = args[1]
 
 # Parse actual and predicted losses from log
+actual_x = []
 actual_loss = []
+predicted_x = []
 predicted_loss = []
 with open(log_file_name) as f:
   cool_lines = [line for line in f.readlines() if "ANDREW" in line]
-  actual_loss = [None] * (len(cool_lines) * 2)
-  predicted_loss = [None] * (len(cool_lines) * 2)
   for line in cool_lines:
     loss = float(line.split()[-1])
-    index = int(re.match(".*ANDREW\((.*)\).*", line).groups()[0])
+    time = int(re.match(".*ANDREW\((.*)\).*", line).groups()[0])
     if "actual" in line:
-      actual_loss[index] = loss
+      actual_x += [time]
+      actual_loss += [loss]
     elif "predicted" in line:
-      predicted_loss[index] = loss
+      predicted_x += [time]
+      predicted_loss += [loss]
     else:
       print "Yikes, bad line:\n\t%s" % line
-  # Now get rid of None entries
-  bad_indices = []
-  for i in range(len(actual_loss)):
-    if actual_loss[i] is None or predicted_loss[i] is None:
-      bad_indices += [i]
-  for i in list(reversed(sorted(bad_indices))):
-    del actual_loss[i]
-    del predicted_loss[i]
 
 # What's the difference between the actual loss and the predicted loss?
-differences = [predicted_loss[i] - actual_loss[i] for i in range(len(actual_loss))]
+differences = []
+for i, pred_loss in enumerate(predicted_loss):
+  time = predicted_x[i]
+  if time in actual_x:
+    j = actual_x.index(time)
+    act_loss = actual_loss[j]
+    differences += [pred_loss - act_loss]
 print "Average difference: %s" % (float(sum(differences)) / len(differences))
 
 # Plot it!
-x = range(len(actual_loss))
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-ax.plot(x, actual_loss, label="actual")
-ax.plot(x, predicted_loss, label="predicted")
-ax.set_xlabel("Epoch")
+ax.plot(actual_x, actual_loss, label="actual")
+ax.plot(predicted_x, predicted_loss, label="predicted")
+ax.set_xlabel("Time (s)")
 ax.set_ylabel("Loss")
 ax.set_title(log_file_name)
 plt.legend()
