a47a435da2d79edb1ea8a7a3ab7533b252eea57d
Andrew Or <andrewor14@gmail.com>
2017-04-16 14:27:56 -0400

    Fix bug in run script

--- run-it.sh
+++ run-it.sh
@@ -13,7 +13,7 @@ function run_spark() {
     --driver-memory 3g\
     --conf spark.executor.memory=50g\
     --conf spark.scheduler.mode=FAIR\
-    "$extra_conf"\
+    $extra_conf\
     --class org.apache.spark.examples.ml.MultilayerPerceptronClassifierExample\
     examples/target/scala-2.11/jars/spark-examples_2.11-2.1.0.jar >& "$log_file_path"
 }
@@ -23,7 +23,7 @@ for window_size in 1 10 50; do
   extra_conf="--conf $conf_prefix.strategy=avg --conf $conf_prefix.avg.windowSize=$window_size"
   log_file_path=/tmp/spark-logs/mlpc_avg_"$window_size".log
   echo "Running avg with window size $window_size, logging to $log_file_path"
-  run_spark(extra_conf, log_file_path)
+  run_spark "$extra_conf" "$log_file_path"
 done
 
 # EWMA
@@ -31,7 +31,7 @@ for alpha in 0.9; do
   extra_conf="--conf $conf_prefix.strategy=ewma --conf $conf_prefix.ewma.alpha=$alpha"
   log_file_path=/tmp/spark-logs/mlpc_ewma_"$alpha".log
   echo "Running ewma with alpha $alpha, logging to $log_file_path"
-  run_spark(extra_conf, log_file_path)
+  run_spark "$extra_conf" "$log_file_path"
 done
 
 # CF
@@ -39,6 +39,6 @@ for decay in 0.95; do
   extra_conf="--conf $conf_prefix.strategy=cf --conf $conf_prefix.cf.decay=$decay"
   log_file_path=/tmp/spark-logs/mlpc_cf_"$decay".log
   echo "Running cf with decay $decay, logging to $log_file_path"
-  run_spark(extra_conf, log_file_path)
+  run_spark "$extra_conf" "$log_file_path"
 done
 
