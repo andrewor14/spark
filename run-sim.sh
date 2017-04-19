02829ef426f7d8ee1fa0765ef0eaa949dd0a7c58
Andrew Or <andrewor14@gmail.com>
2017-04-19 16:40:30 -0400

    Clean up logs further, use new data

--- run-sim.sh
+++ run-sim.sh
@@ -4,7 +4,7 @@ strategy="$1"
 decay="$2"
 fitterName="$3"
 
-log_file_name=sim_mlpc_"$strategy"_"$decay"_"$fitterName"
+log_file_name=sim_mlpc_"$strategy"_"$fitterName"_"$decay"
 
 bin/spark-submit --conf spark.approximation.predLoss.strategy="$strategy" --conf spark.approximation.predLoss.cf.decay="$decay" --conf spark.approximation.predLoss.cf.fitterName="$fitterName" --class org.apache.spark.BestCurveFitterEver my.jar plotting/actual_losses.txt plotting/"$log_file_name".log &> plotting/"$log_file_name".loglog
 
