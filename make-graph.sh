68f0d34d4717cb9e45698bcd651a241226b2c2ff
Andrew Or <andrewor14@gmail.com>
2017-04-20 13:34:38 -0400

    Add logs for fitting 1/x

--- make-graph.sh
+++ make-graph.sh
@@ -3,17 +3,17 @@
 base_dir=$1
 loss_file="$1/losses.txt"
 
-func="OneOverXSquaredFunctionFitter"
+func="OneOverXFunctionFitter"
 
 ./run-sim.sh "$base_dir" avg 1
 ./run-sim.sh "$base_dir" cf 1 $func 10
 ./run-sim.sh "$base_dir" cf 0.5 $func 100
-./plotting/plot.py "$base_dir" \
-  "sim_mlpc_avg_1.log" \
-  "sim_mlpc_cf_"$func"_1.log" \
-  "sim_mlpc_cf_"$func"_0.5.log"
-./plotting/plot_l2.py "$base_dir"
-  "sim_mlpc_avg_1.log" \
-  "sim_mlpc_cf_"$func"_1.log" \
-  "sim_mlpc_cf_"$func"_0.5.log"
+#./plotting/plot.py "$base_dir" \
+#  "sim_mlpc_avg_1.log" \
+#  "sim_mlpc_cf_"$func"_1.log" \
+#  "sim_mlpc_cf_"$func"_0.5.log"
+#./plotting/plot_l2.py "$base_dir"
+#  "sim_mlpc_avg_1.log" \
+#  "sim_mlpc_cf_"$func"_1.log" \
+#  "sim_mlpc_cf_"$func"_0.5.log"
 
