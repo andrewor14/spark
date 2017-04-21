71addf39e1d9537ede91b4be6afd3f9a4d6c7b5d
Andrew Or <andrewor14@gmail.com>
2017-04-21 11:09:26 -0400

    Put 8 algos on one graph using decay = 0.6

--- plotting/plot_l2.py
+++ plotting/plot_l2.py
@@ -7,7 +7,9 @@ import sys
 from os.path import join
 from plot import *
 
-iterations_of_interest = [1, 5, 10]
+iterations_of_interest = [1, 5, 10] # Must be length 3
+curve_to_fit = "one_over_x_squared"
+decay_to_fit = 0.6
 
 def get_data(path):
   l2 = []
@@ -22,11 +24,12 @@ def get_normalized_data(path1, path2, path3):
   data1 = get_data(path1)
   data2 = get_data(path2)
   data3 = get_data(path3)
+  # Normalize everything by the worst error
+  worst_error = max(data1 + data2 + data3)
   for i in range(len(data1)):
-    max_l2 = max(data1[i], data2[i], data3[i])
-    data1[i] = data1[i] / max_l2
-    data2[i] = data2[i] / max_l2
-    data3[i] = data3[i] / max_l2
+    data1[i] = data1[i] / worst_error
+    data2[i] = data2[i] / worst_error
+    data3[i] = data3[i] / worst_error
   return (data1, data2, data3)
 
 def main():
@@ -43,9 +46,9 @@ def main():
   num_bars_per_group = 2 + 3 * num_algorithms
   width = 1.0 / num_bars_per_group
   indices = np.arange(3)
-  log_file_names = ["avg_1.log", "cf_one_over_x_squared_1.log", "cf_one_over_x_squared_0.8.log"]
+  log_file_names = ["avg_1.log", "cf_%s_1.log" % curve_to_fit, "cf_%s_%s.log" % (curve_to_fit, decay_to_fit)]
   empty = [0, 0, 0]
-  color_cycle = ["r", "m", "b", "c", "y", "g", "k"]
+  color_cycle = ["r", "m", "b", "c", "y", "g", "k", "w"]
 
   # Start plotting
   fig, ax = plt.subplots(figsize=(20, 8))
@@ -59,9 +62,11 @@ def main():
     (naive, curve_fitting, curve_fitting_weighted) =\
       get_normalized_data(naive_path, curve_fitting_path, curve_fitting_weighted_path)
     base_indices = indices + 3 * width * i
-    bars += [ax.bar(base_indices + width, naive, width, color=color_cycle[i], hatch="/")]
-    bars += [ax.bar(base_indices + 2 * width, curve_fitting, width, color=color_cycle[i], hatch="-")]
-    bars += [ax.bar(base_indices + 3 * width, curve_fitting_weighted, width, color=color_cycle[i], hatch="\\")]
+    fill_color = color_cycle[i]
+    edge_color = "w" if fill_color == "k" else "k"
+    bars += [ax.bar(base_indices + width, naive, width, color=fill_color, edgecolor=edge_color, hatch="/")]
+    bars += [ax.bar(base_indices + 2 * width, curve_fitting, width, color=fill_color, edgecolor=edge_color, hatch="-")]
+    bars += [ax.bar(base_indices + 3 * width, curve_fitting_weighted, width, color=fill_color, edgecolor=edge_color, hatch="\\")]
   bars += [ax.bar(indices + (3 * num_algorithms + 1) * width, empty, width)]
   ax.set_ylabel("Normalized L2 norm of prediction error")
   ax.set_xlabel("Number of iterations predicted in advance")
@@ -78,7 +83,7 @@ def main():
   box = ax.get_position()
   ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
   ax.legend(legend_bars, legend_names, prop = {"size": 12}, loc = "center left", bbox_to_anchor=(1.04, 0.5))
-  plt.savefig(join(out_dir, "prediction_error_%s.png" % translate_name(curve_fitting_path)))
+  plt.savefig(join(out_dir, "prediction_error_%s.png" % translate_name(log_file_names[-1])))
 
 if __name__ == "__main__":
   main()
