b5e5ed709756cebdba1d7b403dd0a2a58f05fac3
Andrew Or <andrewor14@gmail.com>
2017-04-21 10:50:00 -0400

    Add logs for all algorithms with various decay

--- make-graph.sh
+++ make-graph.sh
@@ -10,11 +10,11 @@ do_the_thing() {
   for decay in 0.6 0.7 0.8 0.9; do
     ./run-sim.sh "$base_dir" cf "$decay" "$func" 100
   done
-  ./plotting/plot.py "$base_dir" \
-    "avg_1.log" \
-    "cf_"$func"_1.log" \
-    "cf_"$func"_0.8.log"
-  ./plotting/plot_l2.py "$base_dir"
+  #./plotting/plot.py "$base_dir" \
+  #  "avg_1.log" \
+  #  "cf_"$func"_1.log" \
+  #  "cf_"$func"_0.8.log"
+  #./plotting/plot_l2.py "$base_dir"
 }
 
 do_the_thing "one_over_x"
