1bb99affc9f14ebb96166b9837f2cd9e694f2993
Andrew Or <andrewor14@gmail.com>
2017-04-30 18:57:31 -0400

    make-graph.sh use only 0.8, 0.9, 1 (decay)

--- make-graph.sh
+++ make-graph.sh
@@ -5,16 +5,15 @@ loss_file="$1/losses.txt"
 
 do_the_thing() {
   func="$1"
-  #./run-sim.sh "$base_dir" avg 1
-  #./run-sim.sh "$base_dir" cf 1 "$func" 100
-  #for decay in 0.6 0.7 0.8 0.9; do
-  #  ./run-sim.sh "$base_dir" cf "$decay" "$func" 100
-  #done
-  ./plotting/plot.py "$base_dir" \
-    "avg_1.log" \
-    "cf_"$func"_1.log" \
-    "cf_"$func"_0.8.log"
-  ./plotting/plot_error.py "$base_dir"
+  ./run-sim.sh "$base_dir" avg 1
+  for decay in 0.8 0.9 1; do
+    ./run-sim.sh "$base_dir" cf "$decay" "$func" 100
+  done
+  #./plotting/plot.py "$base_dir" \
+  #  "avg_1.log" \
+  #  "cf_"$func"_1.log" \
+  #  "cf_"$func"_0.8.log"
+  #./plotting/plot_error.py "$base_dir"
 }
 
 do_the_thing "one_over_x"
